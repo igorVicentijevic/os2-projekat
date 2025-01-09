3d290d594835b66daf436d2224e149a0d4650aa1
igorVicentijevic <igorviceetf@gmail.com>
2025-01-10 00:51:22 +0100

    Initial

---
+++ xv6-riscv-os2-rsicv-raid/user/javni_test.c
@@ -0,0 +1,64 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void check_data(uint blocks, uchar *blk, uint block_size);
+
+int
+main(int argc, char *argv[])
+{
+  //init_raid(RAID1);
+
+  init_raid(RAID0);
+
+  uint disk_num, block_num, block_size;
+  info_raid(&block_num, &block_size, &disk_num);
+  printf("%d %d %d\n",block_num, block_size, disk_num);
+
+  uchar test[1024]="TEST";
+    write_raid(2,test);
+    uchar test2[1024];
+    read_raid(2,test2);
+    printf("test2: %s",test2);
+
+  uint blocks = (512 > block_num ? block_num : 512);
+
+  uchar* blk = malloc(block_size);
+  for (uint i = 0; i < blocks; i++) {
+    for (uint j = 0; j < block_size; j++) {
+      blk[j] = j + i;
+    }
+    //write_raid(i, blk);
+  }
+
+  //check_data(blocks, blk, block_size);
+
+  //disk_fail_raid(2);
+
+  //check_data(blocks, blk, block_size);
+
+  //disk_repaired_raid(2);
+
+  //check_data(blocks, blk, block_size);
+
+  //free(blk);
+
+  //exit(0);
+}
+
+void check_data(uint blocks, uchar *blk, uint block_size)
+{
+  for (uint i = 0; i < blocks; i++)
+  {
+    //read_raid(i, blk);
+    for (uint j = 0; j < block_size; j++)
+    {
+      if ((uchar)(j + i) != blk[j])
+      {
+        printf("expected=%d got=%d", j + i, blk[j]);
+        printf("Data in the block %d faulty\n", i);
+        break;
+      }
+    }
+  }
+}
